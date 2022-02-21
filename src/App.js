fcd73c0d37a67e75bf8d2c643124886cfb032e8d
Gitting better <StevenM82@users.noreply.github.com>
2022-02-21 11:35:21 -0800

    My first react page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Lake({ name }) {
+  return <h1>{name}</h1>;
+}
+
+// I have to figure out how to separate out the components into their
+// own files and import them
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Lake name="Here" />
+        <h1>Archery react app</h1>
+        <h2>Just starting out</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +27,19 @@ function App() {
         >
           Learn React
         </a>
+        <nav>
+          <ul>
+            <li>
+              <a>Home</a>
+            </li>
+            <li>About</li>
+            <li>Contact Us</li>
+          </ul>
+        </nav>
+        <div className="checkbox-align">
+          <label>To do</label>
+          <input type="text"></input>
+        </div>
       </header>
     </div>
   );
